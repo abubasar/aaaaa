9b93ef4e91f713b0b1c693cbdae2308e86af808c
abubasar <ariful.sec@gmail.com>
2019-06-17 18:10:48 +0600

    s

--- Com.A4aero.Ams.DataModel/MarkupPlan.cs
+++ Com.A4aero.Ams.DataModel/MarkupPlan.cs
@@ -6,7 +6,7 @@ namespace Com.A4aero.Ams.DataModel
 {
    public class MarkupPlan:BaseEntity
     {
-        public string Name { get; set; }
+        public string MarkupPlanName { get; set; }
         public virtual ICollection<BusinessEntity> BusinessEntities { get; set; }
     }
 }
